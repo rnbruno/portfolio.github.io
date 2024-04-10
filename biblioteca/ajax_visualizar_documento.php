1dfad8c25297384baea6e8d88004b5528fa1ace4
Bruno Matias <30832455+rnbruno@users.noreply.github.com>
2024-04-09 21:53:25 -0300

    atualizazaqo

--- biblioteca/ajax_visualizar_documento.php
+++ biblioteca/ajax_visualizar_documento.php
@@ -1,25 +1,4 @@
-<?php
-                    // chamar o arquivo de funções
-require_once "../src/tecnico/conf/constantesBanco.php";
-require_once "../src/tecnico/conf/funcoesSemSession.php";
-require_once PATH_CLASSES . "Biblioteca2.php";
 
-
-$retorno['sucesso'] = false;
-$retorno['mensagem'] = toUtf("Erro ao inserir");
-$retorno['debug'] = "";
-
-if ((!is_null(safepost('acervo_id'))) && (safepost('acervo_id') != "")) {
-	$acervo_id = safepost('acervo_id');
-} else {
-	$acervo_id = "";
-}
-
-try {
-	$pesquisa_com_item = new Biblioteca2($pdo);
-	$resultado_geral = $pesquisa_com_item->BuscarTitulosPorAcervoId($acervo_id);
-    $retorno_dados = array();
-	$retorno_dados['buscar_autocomplete'] = array();
 
     foreach ($resultado_geral as $key => $valor) {
 
@@ -38,8 +17,3 @@ try {
 	}
 
 
-} catch (Exception $e) {
-
-}
-echo json_encode($retorno_dados['buscar_autocomplete']);
-?>
